c24ab6d0231fc2874c7938823d994075e8b7b95b
Doct0rLekter <marikwinters@gmail.com>
2023-02-08 18:58:57 -0500

    Learn Rust loops

--- src/main.rs
+++ src/main.rs
@@ -8,10 +8,22 @@ fn get_name() -> String {
     stdin()
         .read_line(&mut your_name)
         .expect("Failed to read line");
-    your_name
+    your_name.trim().to_lowercase()
 }
 
 fn main() {
+    let the_list = ["ben", "jerry", "alice"];
+
     let name = get_name();
-    println!("Hello, {name}");
+    let mut on_the_list = false;
+    for i in &the_list {
+        if i == &name {
+            on_the_list = true;
+        }
+    }
+    if on_the_list {
+        println!("Welcome, {name}!");
+    } else {
+        println!("Get out!");
+    }
 }
