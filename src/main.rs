efe2081e39f64e7bf6ea6a073276eaeb409a9eb9
Doct0rLekter <marikwinters@gmail.com>
2023-02-08 19:38:15 -0500

    Learn Rust struct and iterators

--- src/main.rs
+++ src/main.rs
@@ -12,18 +12,35 @@ fn get_name() -> String {
 }
 
 fn main() {
-    let the_list = ["benji", "ben", "jerry", "alice"];
+    struct Visitor {
+        name: String,
+        greeting: String,
+    }
 
-    let name = get_name();
-    let mut on_the_list = false;
-    for i in &the_list {
-        if i == &name {
-            on_the_list = true;
+    impl Visitor {
+        fn new(name: &str, greeting: &str) -> Self {
+            Self {
+                name: name.to_lowercase(),
+                greeting: greeting.to_string(),
+            }
+        }
+        fn greet_visitor(&self) {
+            println!("{}", self.greeting);
         }
     }
-    if on_the_list {
-        println!("Welcome to the treehouse, {name}!");
-    } else {
-        println!("Get the, f*ck, out of our secret base!");
+
+    let the_list = [
+        Visitor::new("benji", "Hello, Benji. Had enough snacks for the evening?"),
+        Visitor::new("ben", "One word: ice."),
+        Visitor::new("jerry", "One word: cream."),
+        Visitor::new("alice", "Everyone knows you are my favorite."),
+    ];
+
+    let name = get_name();
+    let known_visitor = the_list.iter().find(|visitor| visitor.name == name);
+
+    match known_visitor {
+        Some(visitor) => visitor.greet_visitor(),
+        None => println!("Get the, f*ck, out of our secret base!"),
     }
 }
