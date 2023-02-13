6b3f68eba6d403b65f79ef738c8013e7b92279cb
Doct0rLekter <marikwinters@gmail.com>
2023-02-13 12:13:13 -0500

    Practice using vectors

--- src/main.rs
+++ src/main.rs
@@ -1,17 +1,28 @@
 #![warn(clippy::all, clippy::pedantic)]
 
-use std::io::stdin;
+use std::io::{self, stdin, Write};
 
 fn get_name() -> String {
     let mut your_name = String::new();
-    println!("What's your name?");
+    print!("What's your name? ");
+    io::stdout().flush().unwrap();
     stdin()
         .read_line(&mut your_name)
         .expect("Failed to read line");
     your_name.trim().to_lowercase()
 }
-
+fn get_greeting() -> String {
+    let mut greeting = String::new();
+    print!("How do you want to be greeted? ");
+    io::stdout().flush().unwrap();
+    stdin()
+        .read_line(&mut greeting)
+        .expect("Failed to read line");
+    println!("");
+    greeting.trim().to_lowercase()
+}
 fn main() {
+    #[derive(Debug)]
     struct Visitor {
         name: String,
         greeting: String,
@@ -25,22 +36,31 @@ fn main() {
             }
         }
         fn greet_visitor(&self) {
-            println!("{}", self.greeting);
+            println!("{}\n", self.greeting);
         }
     }
 
-    let the_list = [
+    let mut the_list = vec![
         Visitor::new("benji", "Hello, Benji. Had enough snacks for the evening?"),
         Visitor::new("ben", "One word: ice."),
         Visitor::new("jerry", "One word: cream."),
         Visitor::new("alice", "Everyone knows you are my favorite."),
     ];
 
-    let name = get_name();
-    let known_visitor = the_list.iter().find(|visitor| visitor.name == name);
+    loop {
+        let name = get_name();
+        let known_visitor = the_list.iter().find(|visitor| visitor.name == name);
 
-    match known_visitor {
-        Some(visitor) => visitor.greet_visitor(),
-        None => println!("Get the, f*ck, out of our secret base!"),
+        match known_visitor {
+            Some(visitor) => visitor.greet_visitor(),
+            None => {
+                if name.is_empty() {
+                    break;
+                }
+                println!("{name} is not on \"the list\".\n");
+                the_list.push(Visitor::new(&name, &get_greeting()));
+            }
+        }
     }
+    println!("The final list is: {the_list:#?}");
 }
